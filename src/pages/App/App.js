1b38116d7d9fb5725be511328a769982197671e2
unknown <spontaneousmanji@gmail.com>
2021-10-27 16:07:08 +0100

    feat: implemented CustomButton

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,15 +1,17 @@
 import { Switch, Route } from 'react-router-dom';
 import { path } from '../../routes';
 import CustomInput from '../../components/CustomInput/custom-input';
+import CustomButton from '../../components/CustomButton/custom-button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <CustomInput 
+      {/* <CustomInput 
         type="text"
         placeholder="I am looking for a..."
-      />
+      /> */}
+      <CustomButton />
     </div>
   );
 }
