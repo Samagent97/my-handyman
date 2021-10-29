72066b83e9df0b914593f2be276f0abbe7f0f36d
unknown <spontaneousmanji@gmail.com>
2021-10-29 13:09:33 +0100

    My HandyMan

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,36 +1,14 @@
-//import { Switch, Route } from 'react-router-dom';
-// import { path } from '../../routes';
-// import CustomInput from '../../components/CustomInput/custom-input';
-import CustomButton from '../../components/CustomButton/custom-button';
-import './App.css';
-import ArtisansCard from '../../components/ArtisansCard/artisans-card';
-import Navbar from '../../components/NavMenu/navbar';
-import Footer from '../../components/Footer/Footer';
-import ArtisansCard from '../../components/ArtisansCard/artisans-card';
-import Navbar from '../../components/NavMenu/navbar';
+import { Switch, Route } from 'react-router-dom';
+import { path } from '../../routes';
+
+
 
 
 function App() {
   return (
-    <div className="App">
-      {/* <CustomInput 
-        type="text"
-        placeholder="I am looking for a..."
-      /> */}
-      <CustomButton />
-      <ArtisansCard name='peter godwin' skill='mechanic' phoneno='0807766336' email='jango@email.com'/>
-      <Navbar/>
-      {/* <CustomButton /> */}
-      <Footer />
-      <CustomButton />
-      <ArtisansCard 
-        name='peter godwin' 
-        skill='mechanic' 
-        phoneno='0807766336' 
-        email='jango@email.com'
-      />
-      <Navbar/>
-    </div>
+  <div>
+    <h1>My HandyMan</h1>
+  </div>
   );
 }
 
