8d6e3a8747d9d38a2edc16c529d7fa4cbafe6d24
jothamardel <ardelmbiplang@gmail.com>
2021-10-29 15:30:41 +0100

    chore: resetting application

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,35 +1,11 @@
-//import { Switch, Route } from 'react-router-dom';
-// import { path } from '../../routes';
-// import CustomInput from '../../components/CustomInput/custom-input';
-import CustomButton from '../../components/CustomButton/custom-button';
-import './App.css';
-import ArtisansCard from '../../components/ArtisansCard/artisans-card';
-import Navbar from '../../components/NavMenu/navbar';
-import Footer from '../../components/Footer/Footer';
-import ArtisansCard from '../../components/ArtisansCard/artisans-card';
-import Navbar from '../../components/NavMenu/navbar';
 
+import SampleComponent from '../../components/SampleComponent/sample-component';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* <CustomInput 
-        type="text"
-        placeholder="I am looking for a..."
-      /> */}
-      <CustomButton />
-      <ArtisansCard name='peter godwin' skill='mechanic' phoneno='0807766336' email='jango@email.com'/>
-      <Navbar/>
-      {/* <CustomButton /> */}
-      <Footer />
-      <CustomButton />
-      <ArtisansCard 
-        name='peter godwin' 
-        skill='mechanic' 
-        phoneno='0807766336' 
-        email='jango@email.com'
-      />
-      <Navbar/>
+      <SampleComponent title="My Handy Man"/>
     </div>
   );
 }
