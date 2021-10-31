56f5f27bd9c42355cd8301e1e9498154866524e2
unknown <spontaneousmanji@gmail.com>
2021-10-31 15:26:36 +0100

    feat:Created 3 pages and a Pages stylesheet

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,14 +1,23 @@
-import SampleComponent from '../../components/SampleComponent/sample-component';
+import { render } from '@testing-library/react';
+import { Component } from 'react';
+import Homepage from './Homepage';
+import Register from './Register';
+import Login from './Login'
 import './App.css';
-import Navbar from '../../components/Navbar/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar/>
-      <SampleComponent title="My Handy Man"/>
-    </div>
-  );
+
+class App extends Component {
+   constructor() {
+     super()
+   } 
+   
+   render() {
+return (
+  <div className='App'>
+    <Homepage/>
+  </div>
+   );
+ }
 }
 
 export default App;
---
+++ src/pages/App/Register.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Pages.css'
+
+const Register = ({title,imageUrl,CustomInput,CustomButton}) => {
+  return (
+    <div className='register'>
+      <h1>{title = 'Register'}</h1>
+      <form className='form'></form>
+    </div>
+
+  );
+}
+
+export default Register;
