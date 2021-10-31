cd11bbcba2e21602bc3dd702cdf3dac704d938e7
unknown <spontaneousmanji@gmail.com>
2021-10-31 23:38:07 +0100

    feat:modified 3 files/deleted page.css/created a new page

--- src/components/ArtisansBlogCard/artisansblog-card.js
+++ src/components/ArtisansBlogCard/artisansblog-card.js
@@ -1,29 +1,17 @@
-import './artisansblog.css';
+import './Artisansblog.css';
 
 const ArtisansBlogCard = ({title,imageUrl,body}) => {
     return(
-        <div className="Card-container">
-            <div className= "image-container">
-            <img className='imageb ' src={imageUrl} alt=''/>
-            </div>
-            <div className = "Card-title">
-               <h1>{title}</h1> 
-               </div>
-            <div className = "card-body" >
-                <p>{body}</p>
-            </div>  
-            <div className ="btn">
-                <button>
-                    <a>
-                        Read more 
-                    </a>
-                </button>
-            </div>
-                </div>
-
-        
-        
-
-    );
+    <div>
+        <h1 className='title'>{title}</h1> 
+     <span className="Card-container">
+        <img className='image' src={imageUrl} alt=''/>
+        <p className = "card-description">{body}</p>
+        <button className ="btn">
+            <a> Read more </a>
+        </button>
+    </span>
+  </div>
+  );
 }
 export default ArtisansBlogCard;
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,9 +1,7 @@
 import { Component } from 'react';
 import Homepage from '../Home/Homepage';
-// import Register from '../Register/Register';
-// import Login from '../Login/Login'
 import './App.css';
-import ArtisansBlogCard from '../../components/ArtisansBlogCard/artisansblog-card';
+
 
 
 class App extends Component {
@@ -14,12 +12,7 @@ class App extends Component {
    render() {
 return (
   <div className='App'>
-    <Homepage/>
-    <ArtisansBlogCard
-      title=''
-      imageUrl='https://images.unsplash.com/photo-1470790376778-a9fbc86d70e2?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGJsb2clMjBiYWNrZ3JvdW5kJTIwaW1hZ2VzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
-      body='Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmodtempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Atvero eos et accusam et justo duo dolores et ea rebum.'
-      />
+       <Homepage/>
   </div>
    );
  }
