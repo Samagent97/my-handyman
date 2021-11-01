11df8019f992c462a98513018388557d50746ea0
Goodywrites <ekpot7@gmail.com>
2021-11-01 14:47:25 -0700

    feat:implemented footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,14 +3,12 @@ import './Footer.css';
 const Footer =() =>{
     return (
     <div className='footer'>
-        <li className='link'>
-            <img alt='My HandyMan' src='/'/>
-        </li>
+        
     <ul className='company'>
-        <li className='links'>
+        <li className='footer-links'>
             <a className='mine'to="/">About us</a>
         </li>
-        <li className='links'>
+        <li className='footer-links'>
             <a className='mine'to="/">Terms of use</a>
         </li>
     </ul>
