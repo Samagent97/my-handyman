d065a65869666e1371a4db91063d150ebf66c8f4
Wekavic <victorweka9@gmail.com>
2021-11-02 13:53:39 +0100

    feat:edited blog card in css

--- src/components/BlogCard/blog-card.js
+++ src/components/BlogCard/blog-card.js
@@ -5,11 +5,11 @@ import CustomButton from '../CustomButtton/Custom-button';
 const BlogCard = ({title,imageUrl,description}) => {
     return(
 			<div className="blog-card">
-				<img className='image' src={imageUrl} alt=''/>
+				<img className='image' src={imageUrl} alt=''width='100%' height='20%'/>
 				<div className="card-container">
 						<h1 className='title'>{title}</h1> 
 						<p className = "card-description">{description}</p>
-						<CustomButton title ="Read more" style = {{ background:'#f0794f', color:'white' ,width:'200px'}} />
+						<CustomButton className="btn" title ="Read more" style = {{ background:'#f0794f', color:'white', }}/>
 				</div>
 			</div>
   );
