46c66a3084485363e63df0cd16a839cf5e2fa0c8
Plangprime <plangnanrengshakvwatshak@gmail.com>
2021-11-02 06:25:03 -0700

    feat:implemented button and input

---
+++ src/components/CustomInput/Custom-Input.js
@@ -0,0 +1,10 @@
+import './Custom-Input.css'
+
+const CustomInput = ({style, type , placeholder, textarea}) => {
+    return (
+        <input id ="CustomInput" style = {style} type ={type} placeholder = {placeholder}  >
+           
+        </input>
+    )
+}
+export default CustomInput;
