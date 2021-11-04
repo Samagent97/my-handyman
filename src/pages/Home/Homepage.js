23c5c2fe53942bd9b5b5ebedbdfd25cb911bc3d4
unknown <spontaneousmanji@gmail.com>
2021-11-04 15:34:25 +0100

    chore: commented handy.js and cardlist components

--- src/pages/Home/Homepage.js
+++ src/pages/Home/Homepage.js
@@ -3,7 +3,7 @@ import CustomButton from '../../components/CustomButtton/Custom-button';
 import NavBar from '../../components/Navbar/Navbar';
 import Footer from '../../components/Footer/Footer';
 import CardList from '../../components/CardList/CardList';
-import { handy } from '../../utils/handy';
+// import { handy } from '../../utils/handy';
 import CustomInput from '../../components/CustomInput/Custom-Input'
 import './Homepage.css'
 
@@ -17,7 +17,7 @@ const Homepage = () => {
         <CustomInput  placeholder="I am looking for ..." style={{width:"50%"}} />
         <CustomButton title ="Search" style={{ width: '10%'}} />
       </div>
-      <CardList handy={handy}/>
+      {/* <CardList handy={handy}/> */}
       <Footer/>
     </div>
   );
