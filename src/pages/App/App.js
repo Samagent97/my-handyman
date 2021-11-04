d2cb99e53360ddf86f1cf581e234250c59febc0e
unknown <spontaneousmanji@gmail.com>
2021-11-04 15:06:56 +0100

    chore: updating app.js component

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -6,7 +6,7 @@ import './App.css';
 import {Switch, Route } from 'react-router-dom';
 import Login from '../Login/Login';
 import Register from '../Register/Register';
-import { blogger } from '../../utils/blogger';
+// import { blogger } from '../../utils/blogger';
 import AboutUs from '../AboutUs/AboutUs'
 
 class App extends Component {
@@ -22,8 +22,8 @@ class App extends Component {
           <Route exact path={path.blog} component= {Blog} />
           <Route exact path={path.login} component= {Login} />
           <Route exact path={path.register} component= {Register} />
-          <Route exact path={`${path.viewBlog}/:id`} component={(blogger) => {
-            return <h1>{blogger.match.params.id}</h1>
+          <Route exact path={`${path.viewBlog}/:id`} component={(props) => {
+            return <h1>{props.match.params.id}</h1>
             }} />
             <Route exact path={path.about} component= {AboutUs} />
         </Switch>
