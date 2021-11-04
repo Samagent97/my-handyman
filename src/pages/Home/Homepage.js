5d80b8b2975248b1192503c68e0bc8c224da25ac
Plangprime <plangnanrengshakvwatshak@gmail.com>
2021-11-04 04:33:40 -0700

    feat:responsive search box

--- src/pages/Home/Homepage.js
+++ src/pages/Home/Homepage.js
@@ -4,7 +4,8 @@ import NavBar from '../../components/Navbar/Navbar';
 import Footer from '../../components/Footer/Footer';
 import CardList from '../../components/Card/CardList'
 import { handy } from '../../components/Card/handy';
-import CustomInput from '../../components/CustomInput/Custom-Input'
+import CustomInput from '../../components/CustomInput/Custom-Input';
+import './Homepage.css';
 
 
 const Homepage = () => {
@@ -12,9 +13,10 @@ const Homepage = () => {
     <div className="Header">
       
       <NavBar />
-      <CustomInput  placeholder="I am looking for ..." style={{width:"35.5em"}} />
-
-      <CustomButton title ="Search" style={{width:"7rem"}} />
+      <div className= "responsive-searchbox">
+        <CustomInput  placeholder="I am looking for ..." style={{width:"50%"}} />
+        <CustomButton title ="Search" style={{ width: '10%'}} />
+      </div>
       <CardList handy={handy}/>
       
       <Footer/>
