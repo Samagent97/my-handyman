628e3925c8b31a81c8fc9bcd902c353d8475a1bc
photophactory <ahemenkelvin2@gmail.com>
2021-11-05 13:34:22 +0100

    feat: Updated Contact component

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -10,11 +10,12 @@ const Contact = (props) => {
     return (
       <div className='Contact'>
           <Navbar />
-        <form className='Responsive-contact'>
+        <form className='responsive-contact'>
           <h1 className="header">Contact</h1>
-            <CustomInput type="Email" placeholder='Email' style={{width:"50%", height:"2rem"}}/>
-            <CustomInput  placeholder='Message' style={{width:"50%", height:"18rem"}}/> <br/>
-            <CustomButton title="Submit" style={{width:"40%"}}/>
+          <CustomInput type="text" placeholder='Full Name' style={{width:"50%", height:"2rem"}}/> 
+            <CustomInput type="Email" placeholder='Email' style={{width:"50%", height:"2rem"}}/> <br/>
+           <textarea variant='outlined' placeholder="Message" style={{width:"50%", height:"18rem",}}></textarea> <br/>
+            <CustomButton title="Submit" style={{width:"50%", height:"3rem"}}/>
         </form>
         <Footer />
       </div>
