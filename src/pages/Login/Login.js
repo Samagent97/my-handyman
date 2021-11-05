71153919763ae43b580a4b81513eee49558f8642
Goodywrites <ekpot7@gmail.com>
2021-11-05 13:25:14 -0700

    feat:Implemented Register-page

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import CustomButton from "../../components/CustomButtton/Custom-button";
 import CustomInput from "../../components/CustomInput/Custom-Input";
+import { path } from "../../routes";
 import './Login.css'
 
 const Login = (props) => {
@@ -13,7 +14,7 @@ const Login = (props) => {
           <CustomInput type="password" placeholder='Password' style={{width:"15rem"}}/>
           <CustomButton title="Login"/>
           <p>forgot password?</p>
-        <Link >Don't have an account?Register</Link>
+        <Link to={path.register}>Don't have an account?Register</Link>
       </form>
       <img className="img" src="https://media.istockphoto.com/photos/group-of-young-friends-catching-up-picture-id1243622637?b=1&k=20&m=1243622637&s=170667a&w=0&h=eNN6RjTD_QjDddpxKk5j_jvQE18T-7k1wyd5as_tifw=" alt ="Background"/>
     </div>
