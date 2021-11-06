3517626fb9b0273db6749d4d091921525a6c1e0e
unknown <spontaneousmanji@gmail.com>
2021-11-06 10:20:21 +0100

    feat: updating styles........custom buttons in blog and login page

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -8,17 +8,22 @@ import './Login.css'
 const Login = (props) => {
   return (
     <div className='login'>
-      <form className='form'>
-        <h1 className="header">Login</h1>
-          <CustomInput type="Email" placeholder='Email' style={{width:"15rem"}}/>
-          <CustomInput type="password" placeholder='Password' style={{width:"15rem"}}/>
-          <CustomButton title="Login"/>
-          <p>forgot password?</p>
-        <Link to={path.register} >Don't have an account?Register</Link>
-      </form>
-      <img className="img" src="https://media.istockphoto.com/photos/group-of-young-friends-catching-up-picture-id1243622637?b=1&k=20&m=1243622637&s=170667a&w=0&h=eNN6RjTD_QjDddpxKk5j_jvQE18T-7k1wyd5as_tifw=" alt ="Background"/>
+      <div className="form">
+        <form className="text" method="">
+            <h1>Login</h1>
+                <CustomInput type="Email" placeholder='Email' style={{width:"16rem",padding:"5px"}}/>
+                <br/>
+                <CustomInput type="password" placeholder='Password' style={{width:"16rem",padding:"5px"}}/>
+                <br/>
+                <CustomButton title="Login" style={{width:"13.5rem"}}/>
+                    <p>forgot password?</p>
+                <Link to={path.register} >Don't have an account?Register</Link>
+          </form>
+      </div> 
+        <div className='image'>
+              <img src="https://media.istockphoto.com/photos/group-of-young-friends-catching-up-picture-id1243622637?b=1&k=20&m=1243622637&s=170667a&w=0&h=eNN6RjTD_QjDddpxKk5j_jvQE18T-7k1wyd5as_tifw=" alt ="Background"/>
+        </div>
     </div>
-
   );
 }
 
