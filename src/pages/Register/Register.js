d0aaa2250a5a3948419fc5c49ab2f005b934db64
Goodywrites <ekpot7@gmail.com>
2021-11-08 12:28:44 -0800

    feat:corrections made on register page

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -23,12 +23,12 @@ const Register = (props) => {
           <CustomInput type="password"  placeholder='Confirm Password' style={{width:"16rem",padding:"12px"}}/>
           <br/>
           <CustomButton title="Register" style={{width:"17rem",padding:"12px", height:"3rem"}}/>
-          </div>
            <p>forgot password? </p>
           <Link to={path.login}><p>Already have an account? Login</p></Link>
+          </div>
        </form>
     <div className='background-img'>
-    <img className="img" src="https://media.istockphoto.com/photos/group-of-young-friends-catching-up-picture-id1243622637?b=1&k=20&m=1243622637&s=170667a&w=0&h=eNN6RjTD_QjDddpxKk5j_jvQE18T-7k1wyd5as_tifw=" alt ="Background"/>
+    <img src="https://media.istockphoto.com/photos/group-of-young-friends-catching-up-picture-id1243622637?b=1&k=20&m=1243622637&s=170667a&w=0&h=eNN6RjTD_QjDddpxKk5j_jvQE18T-7k1wyd5as_tifw=" alt ="Background"/>
     </div>
     </div>
 
