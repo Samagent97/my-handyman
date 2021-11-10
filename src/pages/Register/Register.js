1efb655c04a1c0c5782b1236bf3a72d4744a81aa
Goodywrites <ekpot7@gmail.com>
2021-11-10 11:33:59 -0800

    feat:implemented correction on register page

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import CustomButton from "../../components/CustomButtton/Custom-button";
 import CustomInput from "../../components/CustomInput/Custom-Input";
 import { path } from "../../routes";
-import Home from '../../assets/Logo/Home.png'
 import './Register.css'
 import handyMan2 from "../../assets/Images/handyMan2.jpeg";
 
@@ -15,7 +14,7 @@ const Register = (props) => {
 
       <div className='home'>  
         <Link to="/">
-          <svg height="55" viewBox="0 0 21 21" width="21" xmlns="http://www.w3.org/2000/svg"><g fill="white" fill-rule="evenodd" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" transform="translate(1 1)"><path d="m.5 9.5 9-9 9 9"/><path d="m2.5 7.5v7c0 1.1045695.8954305 2 2 2h10c1.1045695 0 2-.8954305 2-2v-7"/></g></svg>
+          <svg height="55" viewBox="0 0 21 21" width="21" xmlns="http://www.w3.org/2000/svg"><g fill="white" fillRule="evenodd" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" transform="translate(1 1)"><path d="m.5 9.5 9-9 9 9"/><path d="m2.5 7.5v7c0 1.1045695.8954305 2 2 2h10c1.1045695 0 2-.8954305 2-2v-7"/></g></svg>
         </Link>
       </div>
 
@@ -38,7 +37,7 @@ const Register = (props) => {
           </div>
        </form>
     <div className='background-img'>
-        <img src={handyMan2} alt="photo"/>
+        <img src={handyMan2} alt="artisan"/>
     </div>
     </div>
 
